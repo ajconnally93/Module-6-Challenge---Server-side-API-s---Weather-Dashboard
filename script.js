7d632a17e363edf66eaf02330440c926ed89c3c3
Andrew Connally <ajconnally93@gmail.com>
2022-11-11 16:56:26 -0600

    added personal API key from openweathermap

---
+++ script.js
@@ -0,0 +1,7 @@
+// empty array and string to be filled in later
+let history = [];
+let lastCity = "";
+const apiKey = "afd214452d5095d525a44dde2fd98431";
+
+// base URL
+// https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
