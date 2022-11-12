996ff9846b8f7dc0268d5ea0c871c56cbb11159c
Andrew Connally <ajconnally93@gmail.com>
2022-11-11 19:27:28 -0600

    used helper code for displayWeather, going to need to help figuring it all out

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const apiKey = "afd214452d5095d525a44dde2fd98431";
 
 let getWeather = function(city) {
     // Need to fix apiUrl
-    let apiUrl = "https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}"
+    let apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=afd214452d5095d525a44dde2fd98431&units=imperial";
     // Need to fix API link
     fetch(apiUrl)
         .then(function(response) {
@@ -36,11 +36,11 @@ let getWeather = function(city) {
 
 let submitHandler = function(event) {
     event.preventDefault();
-    let cityName = $('#cityname').val();
+    let cityName = $('#cityName').val();
     if (cityName) {
         getWeather(cityName);
         // clears search input
-        $('#cityname').val("");
+        $('#cityName').val("");
     } else {
         alert("Error. Please enter a city name.")
     }
@@ -48,4 +48,59 @@ let submitHandler = function(event) {
 
 
 // need to fix API link. Other console logs work properly.
-// getWeather();
+// getWeather(Birmingham);
+
+// need askBCS help to finish filling this in
+let displayWeather = function(weatherData) {
+
+    $("#main-city-name").text(weatherData.name + " (" + dayjs(weatherData.dt * 1000).format("MM/DD/YYYY") + ") ").append(`<img src="https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png"></img>`);
+
+    $("#main-city-temp").text("Temperature: " + weatherData.main.temp.toFixed(1) + " f");
+    $("#main-city-humid").text("Humidity: " + weatherData.main.humidity + "%");
+    $("#main-city-wind").text("Wind Speed: " + weatherData.wind.speed.toFixed(1) + " mph");
+
+    fetch("https://api.openweathermap.org/data/2.5/uvi?lat=" + weatherData.coord.lat + "&lon="+ weatherData.coord.lon + "&appid=afd214452d5095d525a44dde2fd98431")
+        .then(function(response) {
+            response.json().then(function(data) {
+                $("#uv-box").text(data.value);
+
+                if(data.value >= 11) {
+                    $("#uv-box").css("background-color", "purple")
+                } else if (data.value < 11 && data.value >= 8) {
+                    $("#uv-box").css("background-color", "red")
+                } else if (data.value < 8 && data.value >= 6) {
+                    $("#uv-box").css("background-color", "orange")
+                } else if (data.value < 6 && data.value >= 3) {
+                    $("#uv-box").css("background-color", "yellow")
+                } else {
+                    $("#uv-box").css("background-color", "green")
+                }      
+            })
+        });
+
+    fetch("https://api.openweathermap.org/data/2.5/forecast?q=" + weatherData.name + "&appid=afd214452d5095d525a44dde2fd98431&units=imperial")
+        .then(function(response) {
+            response.json().then(function(data) {
+                // clears previous entires in five-day forecast
+                $('#five-day').empty();
+                // every 8th value is a new day
+                for (i = 7; i <= data.list.length; i += 8) {
+                    let fDayCard =`
+                    <div class="col-md-2 m-2 py-3 card text-white bg-primary">
+                        <div class="card-body p-1">
+                            <h5 class="card-title">` + dayjs(data.list[i].dt * 1000).format("MM/DD/YYYY") + `</h5>
+                            <img src="https://openweathermap.org/img/wn/` + data.list[i].weather[0].icon + `.png" alt="rain">
+                            <p class="card-text">Temp: ` + data.list[i].main.temp + `</p>
+                            <p class="card-text">Humidity: ` + data.list[i].main.humidity + `</p>
+                        </div>
+                    </div>
+                    `;
+
+                    $('#five-day').append(fDayCard);
+                }
+            })
+    });
+
+    lastCity = weatherData.name;
+    saveSearchHistory(weatherData.name);
+};
